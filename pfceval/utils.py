e99e063a76510da9fd26a327b9a870a9621823ff
Harun KIVRIL <harun.kivril@knmi.nl>
2025-08-18 11:45:58 +0300

    Documentation for utils

--- pfceval/utils.py
+++ pfceval/utils.py
@@ -4,6 +4,17 @@ from time import time
 
 
 def collect(dataframe, engine="streaming"):
+    """
+    Collects a Polars LazyFrame into a DataFrame if it is lazy.
+
+    Args:
+        dataframe (pl.DataFrame or pl.LazyFrame): The input frame to be collected.
+        engine (str, optional): The engine to use for collection. Defaults to "streaming".
+
+    Returns:
+        pl.DataFrame: A collected DataFrame if the input was LazyFrame; otherwise, 
+            returns the input unchanged.
+    """
     if isinstance(dataframe, pl.LazyFrame):
         start = time()
         dataframe = dataframe.collect(engine=engine)
@@ -13,6 +24,17 @@ def collect(dataframe, engine="streaming"):
 
 
 def collect_all(dataframe_list, engine="streaming"):
+    """
+    Collects a list of Polars LazyFrames into DataFrames if they are lazy.
+
+    Args:
+        dataframe_list (list): A list of LazyFrames or DataFrames.
+        engine (str, optional): The engine to use for collection. Defaults to "streaming".
+
+    Returns:
+        list: A list of collected DataFrames if inputs were LazyFrames; otherwise, 
+            returns the input list unchanged.
+    """
     if isinstance(dataframe_list[0], pl.LazyFrame):
         start = time()
         dataframe_list = pl.collect_all(dataframe_list, engine=engine)
